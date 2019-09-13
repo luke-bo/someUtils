98d268a161816f70b5b9f43f30723bcd44846593
luke.bohonis <w.bohonis@gmail.com>
2019-09-13 14:07:49 -0700

    added power function to utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,11 @@
 function product(num1, num2) {
-    return num1**num2;
+    return num1*num2;
 }
 
 function divide(num1, num2) {
     return num1/num2;
+}
+
+function power(num1, num2) {
+    return num1**num2;
 }
